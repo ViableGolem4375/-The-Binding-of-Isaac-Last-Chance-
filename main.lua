f70be724e1b38d24420795fd0de8de54f356d4b3
ViableGolem4375 <matthewlangton117@gmail.com>
2025-07-19 13:17:02 -0400

    Updated dull coin description.

--- main.lua
+++ main.lua
@@ -2005,7 +2005,7 @@ if EID then
     EID:addIcon("Player"..TAINTED_ABRAHAM_TYPE, "Heretic", 0, 16, 16, 0, 0, EIDicons)
 
     EID:addCollectible(LUCKY_DICE_ID, "Reroll all item pedestals in the current room.#{{Warning}} Items rerolled will be chosen from a special item pool consisting of luck/chance based items.", "Lucky Coin")
-    EID:addCollectible(DULL_COIN_ID, "Rerolls all item pedestals in the room.#Items are rerolled into ones of 1 lower quality.#If no item pedestals containing quality 1 items or above exist and Isaac holds at least 10 quality 0 items, 10 quality 0 items will be removed from his inventory and replaced with a quality 3 or 4 item.", "Dull Coin")
+    EID:addCollectible(DULL_COIN_ID, "Rerolls all item pedestals in the room.#Items are rerolled into ones of 1 lower quality and are guaranteed to be passive items.#If no item pedestals containing quality 1 items or above exist and Isaac holds at least 10 quality 0 items, 10 quality 0 items will be removed from his inventory and replaced with a quality 3 or 4 item.", "Dull Coin")
     EID:addCollectible(HATRED_ITEM, "{{ArrowUp}} 25% chance to become invulnerable briefly upon damaging an enemy.", "Unholy Mantle")
     EID:addCollectible(URIEL_ITEM, "Familiar that trails behind Isaac and preiodically fires a holy light beam forward.#Scales with Isaac's damage.", "Lil' Uriel")
     EID:addCollectible(GABRIEL_ITEM, "Familiar that trails behind Isaac and preiodically fires 4 holy light beams in an 'X' pattern.#Scales with Isaac's damage.", "Lil' Gabriel")
